eddcf3e3adcf58ec515e82ce0b2168163af5d6ad
suyogt <70347530+suyogt@users.noreply.github.com>
2020-09-07 13:28:58 -0400

    Add files via upload

---
+++ Workbook1_1.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd 
+Location = "Downloads\FLPOPULATION.csv"
+df=pd.read_csv(Location, header=None)
+
+
+# In[2]:
+
+
+df.head()
+
+
+# In[ ]:
+
+
+
+
---
+++ Workbook1_2.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pc
+Location = "Downloads\data1.xls"
+df = pc.read_excel(Location)
+
+
+# In[2]:
+
+
+df.head()
+
+
+# In[ ]:
+
+
+
+
